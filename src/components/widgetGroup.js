18777fd2a1deef1809d0f2f207559d947bb62b43
John McCormick <johnmccormick91@gmail.com>
2021-05-14 21:23:43 +0100

    Updates

--- src/components/widgetGroup.js
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-
-export default ({ title, children }) => (
-  <div className="flex flex-col space-y-4 lg:space-y-8 w-full">
-    <h2 className="text-3xl font-bold mb-1">{title}</h2>
-    { children }
-  </div>
-)
